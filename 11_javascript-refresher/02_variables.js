1edb104498ccd832da422e9f7b712f1b17e39845
Kthalv <alvaradokathline@gmail.com>
2025-09-05 11:32:40 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,17 @@
+let name = "Kathline";
+let age = 20;
+let isStudent = true;
+
+console.log(name, age, isStudent);
+console.log(typeof name, typeof age, typeof isStudent);
+
+let a = 10;
+let b = 5;
+console.log("Add:", a + b);
+console.log("Subtract", a - b);
+console.log("Multiply:", a * b);
+console.log("Divide:", a / b);
+
+console.log("5" == 5); // true (loose equality)
+console.log("5" === 5); // false (strict equality)
+

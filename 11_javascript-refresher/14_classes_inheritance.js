039d5081f3ed4dccf141efb6b5dd3a81d49c91d2
Kthalv <alvaradokathline@gmail.com>
2025-09-05 11:40:32 +0800

    Add classes and inheritance activity

---
+++ 11_javascript-refresher/14_classes_inheritance.js
@@ -0,0 +1,19 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+sayHello() {
+    console.log("Hi, I am " + this.name);
+    }
+}
+
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+const student1 = new Student("Kathline");
+student1.sayHello();
+student1.study();

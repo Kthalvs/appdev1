f8de500aeb7f3bba7c3b50dd43a83a31d5aa72ad
Kthalv <alvaradokathline@gmail.com>
2025-09-05 11:42:31 +0800

    Add modules export activity

---
+++ 11_javascript-refresher/15_modules_export.js
@@ -0,0 +1,10 @@
+// Default export: a greeting function
+export default function greeting() {
+    return "Hello from the module!";
+}
+
+// Named export: personal info
+export const info = {
+    name: "Kathline",
+    age: 20
+};

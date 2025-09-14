ca452bbbcda34a97b906615aae7a0fea57461990
Kthalv <alvaradokathline@gmail.com>
2025-09-14 17:44:25 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// User data
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+// Button component
 function MyButton() {
   return (
   <button>I'm a Button</button>
   );
 }
 
+// About component
 function AboutPage() {
   return (
     <>
@@ -17,15 +26,29 @@ function AboutPage() {
     </>
   );
 }
-
+ 
+// Main component
 export default function App() {
   return (
     <div>
       <h1>Welcome to my app</h1>
       <MyButton/>
       <AboutPage />
+
+ {/* Showing user data below */}
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </div>
   );
 }
+
 
 

31a76f21e4ff2ba94cc8bfcc0f51fa59bf5b3c2d
Kthalv <alvaradokathline@gmail.com>
2025-11-05 11:15:16 +0800

    Done with why understanding your UI as trees is useful

---
+++ react-describing-ui/src/RootComponent.jsx
@@ -0,0 +1,16 @@
+import ComponentA from './components/ComponentA';
+import ComponentC from './components/ComponentC';
+
+
+function RootComponent() {
+  return (
+    <div>
+      <h1>Root Component</h1>
+      <ComponentA />
+      <ComponentC />
+    </div>
+  );
+}
+
+
+export default RootComponent;

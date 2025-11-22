4a5220eabcb81c3db584aece8e910abc90e69152
Kthalv <alvaradokathline@gmail.com>
2025-11-22 14:48:41 +0800

    Done with more fun with JavaScript objects and curly braces

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,16 +1,31 @@
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageUrl: 'https://i.imgur.com/7vQD0fPs.jpg',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink',
+    padding: '20px'
+  }
+};
+
 export default function App() {
   return (
-    <ul
-      style={{
-        backgroundColor: 'black',
-        color: 'pink',
-        padding: '15px'
-      }}
-    >
-      <h2>Using double curlies: CSS and other objects in JSX</h2>
-      <li>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
-      <li>Work on the alcohol-fuelled engine</li>
-    </ul>
+    <div style={person.theme}>
+      <h2>More fun with JavaScript objects and curly braces</h2>
+      <h1>{person.name}'s Todos</h1>
+
+      <img
+        className="avatar"
+        src={person.imageUrl}
+        alt={person.name}
+      />
+
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
   );
 }
+

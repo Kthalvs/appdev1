acefa563adf6a0ccbc79059fa3cfd300bdb02fbb
Kthalv <alvaradokathline@gmail.com>
2025-11-22 14:37:20 +0800

    Done with passing strings with quotes

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div>
+      <h2>Passing strings with quotes</h2>
+
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+
+      <p>"Strings can be written inside quotes"</p>
+    </div>
+  );
+}

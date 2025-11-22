bac5cccf119687701af8c192ac6a9b98c1ae287d
Kthalv <alvaradokathline@gmail.com>
2025-11-22 14:46:15 +0800

    Done with using double curlies: CSS and other objects in JSX

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,10 +1,16 @@
-const name = "Gregorio Y. Zara";
-
 export default function App() {
   return (
-    <div>
-      <h2>Using curly braces: A window into the JavaScript world</h2>
-      <h1>{name}'s To Do List</h1>
-    </div>
+    <ul
+      style={{
+        backgroundColor: 'black',
+        color: 'pink',
+        padding: '15px'
+      }}
+    >
+      <h2>Using double curlies: CSS and other objects in JSX</h2>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
   );
 }
